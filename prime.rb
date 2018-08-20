15eaad1f55090ebe9f316cd57dcd7ea19ea54ef0
braj011 <bhagyaraj01@gmail.com>
2018-08-20 10:53:17 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,8 @@
 # Add  code here!
 
-
+def prime?
+  
+end 
 
 describe "prime?" do
   it 'returns true for prime numbers' do
@@ -8,3 +10,14 @@ describe "prime?" do
     expect(prime?(11)).to be(true)
     expect(prime?(105557)).to be(true)
   end
+  
+  it 'returns false for non-prime numbers' do
+    expect(prime?(-1)).to be(false), "Be sure to account for negative numbers!"
+    expect(prime?(0)).to be(false)
+    expect(prime?(1)).to be(false)
+    expect(prime?(4)).to be(false)
+    expect(prime?(40)).to be(false)
+    expect(prime?(1763)).to be(false)
+    expect(prime?(101013)).to be(false)
+  end
+end
