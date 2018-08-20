9bcf567c889e5237fc916aacc21a7bd89d965bcb
braj011 <bhagyaraj01@gmail.com>
2018-08-20 11:09:13 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,23 +1,15 @@
 # Add  code here!
 
-def prime?(int) 
-  
-end 
 
-describe "prime?" do
-  it 'returns true for prime numbers' do
-    expect(prime?(3)).to be(true)
-    expect(prime?(11)).to be(true)
-    expect(prime?(105557)).to be(true)
-  end
-  
-  it 'returns false for non-prime numbers' do
-    expect(prime?(-1)).to be(false), "Be sure to account for negative numbers!"
-    expect(prime?(0)).to be(false)
-    expect(prime?(1)).to be(false)
-    expect(prime?(4)).to be(false)
-    expect(prime?(40)).to be(false)
-    expect(prime?(1763)).to be(false)
-    expect(prime?(101013)).to be(false)
+
+
+def prime?(n)
+  2.upto(Math.sqrt(n)) do |x| 
+    if n % x == 0
+      return false # this means the number is not prime
+    else
+      return true # this means the number is prime
+    end 
   end
 end
+
