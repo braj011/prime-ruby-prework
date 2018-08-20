9b201350db18ea9b42bdaa867306386e397b060f
braj011 <bhagyaraj01@gmail.com>
2018-08-20 10:52:37 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+
+
+
+describe "prime?" do
+  it 'returns true for prime numbers' do
+    expect(prime?(3)).to be(true)
+    expect(prime?(11)).to be(true)
+    expect(prime?(105557)).to be(true)
+  end
